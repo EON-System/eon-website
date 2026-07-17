a239e7d1094f57dd4caf98c899e5736ef6640432
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-07-17 14:11:38 +0300

    Create constants.ts

---
+++ src/lib/constants.ts
@@ -0,0 +1,10 @@
+export const COMPANY_NAME = "EON AI";
+
+export const COMPANY_EMAIL =
+  "eon.ai.inc@gmail.com";
+
+export const COMPANY_SLOGAN =
+  "Building AI companions for human evolution.";
+
+export const COMPANY_DESCRIPTION =
+  "Creating the world's best personal AI products.";
