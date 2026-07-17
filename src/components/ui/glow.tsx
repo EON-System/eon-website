f648171b7ace6b3aede13951471d1acf3d73ee2b
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-07-17 23:25:14 +0300

    Create glow.tsx

---
+++ src/components/ui/glow.tsx
@@ -0,0 +1,9 @@
+export function Glow() {
+  return (
+    <>
+      <div className="absolute left-1/2 top-0 h-[500px] w-[500px] -translate-x-1/2 rounded-full bg-[#D6B25E]/10 blur-[140px]" />
+
+      <div className="absolute bottom-0 right-0 h-[400px] w-[400px] rounded-full bg-[#8B5CF6]/10 blur-[160px]" />
+    </>
+  );
+}
