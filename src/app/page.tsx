50b16e77710251009c72ae2d71c35faf20d455c9
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-07-17 14:25:20 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,14 @@ import { CoreSection } from "@/components/sections/core";
 import { MissionSection } from "@/components/sections/mission";
 import { ContactSection } from "@/components/sections/contact";
 import { TechnologySection } from "@/components/sections/technology";
+import { FounderSection } from "@/components/sections/founder";
 
 export default function HomePage() {
   return (
     <main className="bg-[#050505] text-white overflow-hidden">
       <HeroSection />
       <VisionSection />
+      <FounderSection />
       <ProductsSection />
       <TechnologySection />
       <CoreSection />
