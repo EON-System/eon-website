8d8beae80f39201444f164f450e29c406507666f
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-07-17 13:47:54 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Footer } from "@/components/layout/footer";
 
 import { Navbar } from "@/components/layout/navbar";
 import { Background } from "@/components/core/background";
@@ -47,6 +48,8 @@ export default function RootLayout({
         <Navbar />
 
         {children}
+
+        <Footer />
 
       </body>
     </html>
