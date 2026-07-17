07792ab500ecbf3ce1f544f6d754e739b08289ae
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-07-17 23:44:42 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { HeroSection } from "@/components/sections/hero-section";
+
 import { AboutV2 } from "@/components/sections/about-v2";
 import { ResearchV2 } from "@/components/sections/research-v2";
 import { ProductsV2 } from "@/components/sections/products-v2";
 import { CompanyV2 } from "@/components/sections/company-v2";
 import { VisionV2 } from "@/components/sections/vision-v2";
+
+import { ContactV2 } from "@/components/sections/contact-v2";
+import { CareersV2 } from "@/components/sections/careers-v2";
 
 
 export default function Home() {
@@ -21,6 +25,10 @@ export default function Home() {
       <CompanyV2 />
 
       <VisionV2 />
+
+      <CareersV2 />
+
+      <ContactV2 />
 
     </main>
   );
