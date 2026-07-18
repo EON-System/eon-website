25316d3c14842cb47a4537e6cf5440e799a6d70f
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-07-18 09:17:54 +0300

    Update container.tsx

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -30,11 +30,14 @@ export function Container({
         mx-auto
         w-full
         max-w-7xl
+
         px-5
 
         sm:px-8
 
         lg:px-12
+
+        xl:px-16
         `,
 
         className
