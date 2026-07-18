c386a32aec29d91877707978f51720860834a6b8
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-07-18 09:16:59 +0300

    Update container.tsx

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -1,26 +1,52 @@
 import { cn } from "@/lib/utils";
 
 
-type ContainerProps = {
+interface ContainerProps {
+
   children: React.ReactNode;
 
   className?: string;
-};
+
+}
+
 
 
 export function Container({
+
   children,
+
   className,
+
 }: ContainerProps) {
 
+
   return (
+
     <div
+
       className={cn(
-        "mx-auto w-full max-w-7xl px-6 lg:px-8",
+
+        `
+        mx-auto
+        w-full
+        max-w-7xl
+        px-5
+
+        sm:px-8
+
+        lg:px-12
+        `,
+
         className
+
       )}
+
     >
+
       {children}
+
     </div>
+
   );
+
 }
