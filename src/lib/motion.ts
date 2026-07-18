95f990d89a2efa1624a6bb5248b7e1385d90096e
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-07-18 09:19:40 +0300

    Create motion.ts

---
+++ src/lib/motion.ts
@@ -0,0 +1,21 @@
+export const motionConfig = {
+
+  transition: {
+
+    duration: 1,
+
+    ease: [0.22, 1, 0.36, 1],
+
+  },
+
+
+  viewport: {
+
+    once: true,
+
+    margin: "-120px",
+
+  },
+
+
+};
