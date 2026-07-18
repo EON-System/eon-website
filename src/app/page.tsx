8d11b883cd49925838fe53e56e17f76c9adf072a
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-07-18 09:14:48 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,42 @@
-import { HeroSection } from "@/components/sections/hero-section";
+import { Hero } from "@/components/hero/hero";
 
 import { AboutV2 } from "@/components/sections/about-v2";
-import { ResearchV2 } from "@/components/sections/research-v2";
 import { ProductsV2 } from "@/components/sections/products-v2";
-import { CompanyV2 } from "@/components/sections/company-v2";
-import { VisionV2 } from "@/components/sections/vision-v2";
-
+import { ResearchV2 } from "@/components/sections/research-v2";
+import { NewsV2 } from "@/components/sections/news-v2";
 import { ContactV2 } from "@/components/sections/contact-v2";
-import { CareersV2 } from "@/components/sections/careers-v2";
 
 
 export default function Home() {
-  return (
-    <main>
 
-      <HeroSection />
+
+  return (
+
+    <main className="
+      overflow-hidden
+    ">
+
+
+      <Hero />
+
 
       <AboutV2 />
 
-      <ResearchV2 />
 
       <ProductsV2 />
 
-      <CompanyV2 />
 
-      <VisionV2 />
+      <ResearchV2 />
 
-      <CareersV2 />
+
+      <NewsV2 />
+
 
       <ContactV2 />
 
+
     </main>
+
   );
+
 }
