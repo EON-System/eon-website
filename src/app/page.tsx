1298740b05c8d55d617390b55194b1b1292b4c22
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-07-28 17:29:57 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import { IntelligenceEvolution } from "@/components/intelligence-evolution";
 import { FutureIntelligence } from "@/components/future-intelligence";
 
 import { AboutV2 } from "@/components/sections/about-v2";
-import { ProductsV2 } from "@/components/sections/products-v2";
 import { ResearchV2 } from "@/components/sections/research-v2";
 import { NewsV2 } from "@/components/sections/news-v2";
 import { ContactV2 } from "@/components/sections/contact-v2";
@@ -23,29 +22,42 @@ export default function Home() {
       <Hero />
 
 
+      {/* Product universe */}
+
       <ProductShowcase />
 
+
+      {/* Philosophy */}
 
       <EonIntelligence />
 
 
+      {/* AI evolution story */}
+
       <IntelligenceEvolution />
 
 
-      <FutureIntelligence />
-
+      {/* Company vision */}
 
       <AboutV2 />
 
 
-      <ProductsV2 />
-
+      {/* Research */}
 
       <ResearchV2 />
 
 
+      {/* Future statement */}
+
+      <FutureIntelligence />
+
+
+      {/* Updates */}
+
       <NewsV2 />
 
+
+      {/* Contact */}
 
       <ContactV2 />
 
