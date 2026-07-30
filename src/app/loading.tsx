ebf12215c010784e5e446d9303b8d6596558dc6b
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-07-30 20:55:37 +0300

    Update loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,25 +1,5 @@
+import { CinematicLoader } from "@/components/loading/CinematicLoader";
+
 export default function Loading() {
-
-  return (
-
-    <div className="flex min-h-screen items-center justify-center bg-[#050505]">
-
-      <div className="text-center">
-
-
-        <div className="text-4xl font-semibold text-[#D6B25E]">
-          EON AI
-        </div>
-
-
-        <div className="mt-4 text-sm uppercase tracking-[0.4em] text-white/40">
-          Loading Intelligence
-        </div>
-
-
-      </div>
-
-    </div>
-
-  );
+  return <CinematicLoader />;
 }
